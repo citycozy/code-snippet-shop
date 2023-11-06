f3826fa0d002961d8cbe62a24c9f5085e5edb2f0
citycozyboy <citycozyboy@gmail.com>
2023-11-06 17:34:48 +0900

    Initial commit

---
+++ src/main/java/com/sku/codesnippetshop/CodeSnippetShopApplication.java
@@ -0,0 +1,13 @@
+package com.sku.codesnippetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSnippetShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSnippetShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sku/codesnippetshop/CodeSnippetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sku.codesnippetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSnippetShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
