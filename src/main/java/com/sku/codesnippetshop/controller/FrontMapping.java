2c56cb60921d5b1bb6624e3b9f692d5a0fc20ab3
NORTH205-19\북205_20 <knkn9436@naver.com>
2023-11-07 10:38:26 +0900

    Feat. 프론트 레이아웃 구성(로그인, 회원가입, 메인, 상품 상세), 임시 FrontMapping 컨트롤러

---
+++ src/main/java/com/sku/codesnippetshop/controller/FrontMapping.java
@@ -0,0 +1,29 @@
+package com.sku.codesnippetshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontMapping {
+
+    @GetMapping("product-detail")
+    public String productDetail(){
+        return "/products/detail";
+    }
+    @GetMapping("login")
+    public String login(){
+        return "/login/login";
+    }
+
+    @GetMapping("join")
+    public String join(){
+        return "/join/join";
+    }
+
+    @GetMapping("cart")
+    public String cart(){
+        return "/products/cart";
+    }
+
+
+}
